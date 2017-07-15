9b75b70cbd6b7bdbee5427e71b3f4ad912b9c342
karimb <karimboumedhel@gmail.com>
2017-07-15 03:24:26 +0200

    correct troll listing

--- nickyabbot.py
+++ nickyabbot.py
@@ -10,7 +10,7 @@ def db_setup(dbfile='troll.db'):
     cursor.execute('DELETE FROM quotes WHERE chatid = 0')
     with open('staticquotes.txt', 'r') as staticquotes:
         for line in staticquotes:
-            cursor.execute('''INSERT INTO quotes(chatid,quote) VALUES(?,?)''', (0, line))
+            cursor.execute('''INSERT INTO quotes(chatid,quote) VALUES(?,?)''', (0, line.strip()))
     db.commit()
     db.close()
 
@@ -22,36 +22,92 @@ if 'TOKEN' not in os.environ:
 bot = telebot.TeleBot(os.environ['TOKEN'])
 
 
+@bot.message_handler(commands=['trollhelp'])
+def help(message):
+    helpmessage = '''Those are the commands available
+    /troll          Displays a random troll ( or finds the one matching provided string)
+    /trolladd       Adds the indicated troll
+    /trolldelete    Deletes given troll
+    /trolllis       Lists all the trolls
+    /trollhelp      This help
+    '''
+    bot.reply_to(message, helpmessage)
+
+
 @bot.message_handler(commands=['troll'])
-def random(message):
+def get(message):
+    quote = message.text.replace('/troll', '').strip()
     chatid = message.chat.id if message.chat.title is not None else 0
     db = sqlite3.connect('troll.db')
     cursor = db.cursor()
-    cursor.execute('''SELECT quote FROM quotes WHERE chatid = ? OR chatid = 0 ORDER BY RANDOM() LIMIT 1''', (chatid,))
-    randomquote = cursor.fetchone()[0]
+    if quote == '':
+        cursor.execute('''SELECT quote FROM quotes WHERE chatid = ? OR chatid = 0 ORDER BY RANDOM() LIMIT 1''', (chatid,))
+        fetch = cursor.fetchone()
+    else:
+        cursor.execute("SELECT quote FROM quotes WHERE quote like ? AND (chatid = ? OR chatid = 0)", ('%' + quote + '%', chatid,))
+        fetch = cursor.fetchone()
+        if fetch is None:
+            print("No Matching quote found")
+            bot.reply_to(message, 'No Matching quote found')
+            return
+    quote = fetch[0]
     db.close()
-    print("Sending Troll message...")
-    bot.reply_to(message, randomquote)
+    print("Sending troll message to user %s" % message.from_user.username)
+    bot.reply_to(message, quote)
+
+
+@bot.message_handler(commands=['trollall', 'trolllist'])
+def all(message):
+    db = sqlite3.connect('troll.db')
+    cursor = db.cursor()
+    if message.chat.type == 'group':
+        cursor.execute('SELECT quote FROM quotes where chatid = ? or chatid = 0', (message.chat.id,))
+    else:
+        cursor.execute('SELECT quote FROM quotes where chatid = 0')
+    quotes = '\n'.join([q[0] for q in cursor.fetchall()])
+    db.close()
+    print("Sending all troll messages to user %s" % message.from_user.username)
+    bot.reply_to(message, quotes)
 
 
 @bot.message_handler(commands=['trolladd'])
 def add(message):
     quote = message.text.replace('/trolladd ', '')
     if quote == '':
-        bot.reply_to(message, 'Missing Troll text to add')
+        bot.reply_to(message, 'Missing troll text to add')
         return
-    if message.chat.title is None:
-        bot.reply_to(message, 'Trolls can only be added to channels')
+    quote = quote.strip()
+    if message.chat.type != 'group':
+        bot.reply_to(message, 'Trolls can only be added to groups')
         return
     db = sqlite3.connect('troll.db')
     cursor = db.cursor()
     cursor.execute('''INSERT INTO quotes(chatid,quote) VALUES(?,?)''', (message.chat.id, quote))
-    print("Adding Troll message to chat %s" % message.chat.title)
+    print("Adding Troll message to group %s" % message.chat.title)
     bot.reply_to(message, 'Troll added to your channel')
-    cursor.execute('''SELECT * FROM quotes''')
-    allrows = cursor.fetchall()
-    for q in allrows:
-        print q
+    db.commit()
+    db.close()
+
+
+@bot.message_handler(commands=['trolldel', 'trolldelete'])
+def delete(message):
+    quote = message.text.replace('/trolldelete ', '').replace('/trolldel', '').strip()
+    print quote
+    if quote == '':
+        bot.reply_to(message, 'Missing troll text to delete')
+        return
+    if message.chat.type != 'group':
+        bot.reply_to(message, 'Trolls can only be deleted from groups')
+        return
+    db = sqlite3.connect('troll.db')
+    cursor = db.cursor()
+    deleted = cursor.execute('''DELETE FROM QUOTES WHERE chatid = ? and  quote = ?''', (message.chat.id, quote))
+    if deleted.rowcount > 0:
+        print("Deleted Troll message from group %s" % message.chat.title)
+        bot.reply_to(message, 'Troll deleted from your channel')
+    else:
+        print("No Troll message deleted from group %s" % message.chat.title)
+        bot.reply_to(message, 'Troll was not deleted from your group')
     db.commit()
     db.close()
 
